622460e6e210e1c51b56f723d373998e8a032e34
Denisa <denibel04@gmail.com>
2022-10-06 10:11:48 +0200

    ej2 print

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,8 +1,7 @@
-public class Main {
+public class Ejercicio1 {
     public static void main(String[] args) {
  
         System.out.println("Denisa Ramona Belean");
     }
  
  }
- 
--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,7 +1,8 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+ 
+        System.out.println("Mi nombre es Denisa Ramona Belean\nVivo en calle Valle de Abdalajís\nMi número de teléfono es 643609830");
     }
-}
-
-// XD
+ 
+ }
+ 
